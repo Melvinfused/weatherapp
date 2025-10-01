639b758ef042f207440947277375d52a404be8d8
Melvin Francy <thomasfrancymoothedath0@gmail.com>
2025-10-01 14:42:16 +0530

    Updated WeatherInfoCard about & ui elements

--- src/about.jsx
+++ src/about.jsx
@@ -7,13 +7,12 @@ const AboutCard = () => {
 <div className="about-overlay">
   <div className="about-card">
     <h2 className="about-header">Weather</h2>
-    <p className="about-paragraph">
-      Ah, so thou hast found this Weather App, and perchance thou art wondering, “What makes this so special?” Well, let me tell thee. ‘Tis quick, ‘tis sleek, and gives thee only what thou needest—no nonsense, no distractions. It is the app thou deservest, though thou may not realise it yet... but trust me, thou most certainly need it!
+    <p className="about-paragraph" style={{ fontFamily: '"Jacquard 24", system-ui',fontSize: "16px" }}>
+    Ah, thou hast come upon this weather-telling instrument, and perchance thou dost wonder, ‘What maketh it so fine?’ Know this: it is swift, simple, and showeth thee only that which thou needest—no idle trifles, no vain matters. ‘Tis the very tool thou wert meant to have, though thou mayst not yet ken it… trust me, thou shalt find it most needful!    </p>
+    <p className="about-paragraph" style={{ fontFamily: '"Jacquard 24", system-ui',fontSize: "16px" }}>
+      Just a weather teller with a bit more rizz.
     </p>
-    <p className="about-paragraph">
-      Just a weather app with a bit more rizz.
-    </p>
-    <div className="about-footer">
+    <div className="about-footer" style={{ fontFamily: '"Jacquard 24", system-ui',fontSize: "18px" }}>
       <p>Crafted with far too much free time, <br />faith & a GPT 4o by Melvin Francy</p>
     </div>
   </div>
